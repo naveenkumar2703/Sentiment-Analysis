6f63550e33d88ee48300d9e1afd921005a235859
Naveen <naveenkumar2703@gmail.com>
2016-04-06 23:32:10 -0400

    File to filter business with most reviews

---
+++ code/Popular_business_identifier.py
@@ -0,0 +1,34 @@
+"""@author:Naveen. This file simpy picks business that have atleast minNumberOfReviews from srcFolder to destFolder"""
+from os import listdir
+from os.path import isfile, join
+import os
+import shutil
+
+
+srcFolder = '/Users/naveenkumar2703/GitHub/Sentiment-Analysis/Data/yelp-dataset/'
+destFolder = '/Users/naveenkumar2703/GitHub/Sentiment-Analysis/Data/Popular business/'
+minNumberOfReviews = 50
+
+def countNumberOfLines(fileName):
+    file = open(fileName,'r')
+    for numberOfLines,line in enumerate(file):
+        pass
+    return numberOfLines+1
+
+def getPopularBusinessIds():
+    popularBusiness = []
+
+    for item in os.listdir(str(srcFolder)):
+        if(item.endswith('.txt') and countNumberOfLines(str(srcFolder+item)) > (minNumberOfReviews-1)):
+            popularBusiness.append(item)
+    print(len(popularBusiness))
+    return popularBusiness
+           
+           
+def copyPopularBusiness():
+    for item in getPopularBusinessIds():
+        shutil.copy(str(srcFolder+item),destFolder)
+
+           
+copyPopularBusiness()
+
